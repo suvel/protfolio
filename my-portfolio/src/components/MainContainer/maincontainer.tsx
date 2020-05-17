807f38012359d7513c2afe429e87a8f6d85adc23
SuvelReatneshwar <find.suvel@gmail.com>
2020-05-17 23:40:45 +0530

    added media query for responsive mobile/smart phones (480px)

--- my-portfolio/src/components/MainContainer/maincontainer.tsx
+++ my-portfolio/src/components/MainContainer/maincontainer.tsx
@@ -1,13 +1,10 @@
 import React, { FunctionComponent } from "react";
 import "./maincontainer.css";
 
-const background = require("../../assets/images/man-bg.svg");
-
 const MainContainer: FunctionComponent = (props) => {
   return (
     <div
       className={"main-container"}
-      style={{ backgroundImage: `url(${background})` }}
     >
       {props.children}
     </div>
